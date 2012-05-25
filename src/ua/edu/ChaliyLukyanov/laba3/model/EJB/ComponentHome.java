1da8992cb0bf8746fb6b58cc22e1f51bfc1194d5
Yura Lukyanov <lukyanov.yura@gmail.com>
2012-05-25 11:52:14 +0300

    delete

--- src/ua/edu/ChaliyLukyanov/laba3/model/EJB/ComponentHome.java
+++
@@ -1,9 +0,0 @@
-package ua.edu.ChaliyLukyanov.laba3.model.EJB;
-
-import java.rmi.RemoteException;
-import javax.ejb.CreateException;
-import javax.ejb.EJBHome;
-
-public interface ComponentHome extends EJBHome {
-	ComponentRemote create() throws RemoteException, CreateException;
-}
--- src/ua/edu/ChaliyLukyanov/laba3/model/EJB/DeviceHome.java
+++
@@ -1,9 +0,0 @@
-package ua.edu.ChaliyLukyanov.laba3.model.EJB;
-
-import java.rmi.RemoteException;
-import javax.ejb.CreateException;
-import javax.ejb.EJBHome;
-
-public interface DeviceHome extends EJBHome {
-	DeviceRemote create() throws RemoteException, CreateException;
-}
